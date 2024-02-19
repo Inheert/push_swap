dce4c1f29c81f149ea5739f436100b22b8e5ec91
Inheert <theoclaereboudt@gmail.com>
2024-02-19 15:00:19 +0100

    Files created

--- push/push_swap.c
+++ push/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: tclaereb <tclaereb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 17:14:46 by tclaereb          #+#    #+#             */
-/*   Updated: 2024/02/19 13:24:47 by tclaereb         ###   ########.fr       */
+/*   Updated: 2024/02/19 14:15:08 by tclaereb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,10 +60,24 @@ void	exception(t_list **stack_a, t_list **stack_b)
 t_list	*check_args(int argc, char **argv)
 {
 	t_list	*stack_a;
+	t_list	*to_check;
+	t_list	*checker;
 
 	if (argc <= 1)
-		exception(NULL, NULL);
+		exit(0);
 	stack_a = ft_lstnew(-1);
 	fill_stack(argv, stack_a);
+	to_check = stack_a;
+	while (to_check)
+	{
+		checker = stack_a;
+		while (checker)
+		{
+			if (checker->content == to_check->content && checker != to_check)
+				exception(&stack_a, NULL);
+			checker = checker->next;
+		}
+		to_check = to_check->next;
+	}
 	return (stack_a);
 }
